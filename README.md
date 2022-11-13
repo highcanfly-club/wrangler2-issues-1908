c603ec16d19a3665f5dbf5a7a095131a83ed4d22
Ronan LE MEILLAT <eltorio@sctg.eu.org>
2022-11-13 12:58:30 +0100

    ugly workaround

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Vue 3 + TypeScript + Vite + Wrangler + SSL
 
-This project is just for reproducing https://github.com/cloudflare/wrangler2/issues/1908
+This project is just for reproducing https://github.com/cloudflare/wrangler2/issues/1908  
+This branch implements a ugly workaroud.  
+```sh
+#produce a cert chain
+npm run create-cert
+#run wrangler allowing previously produced CA cert
+npm run dev
+#browse https://localhost:8788
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
+/*!
+=========================================================
+* © 2022 Ronan LE MEILLAT for SCTG Développement
+=========================================================
+This website use:
+- Vite, Vue3, Cloudflare
+- And many others
+*/
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import fs from 'fs'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [basicSsl(),vue()],
-  server: {https: true}
+  plugins: [
+    fs.existsSync("./localhost.key") ? vue():[basicSsl(),vue()],
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '~': path.resolve(__dirname, './node_modules'),
+      '§': path.resolve(__dirname, './'),
+    },
+  },
+  server: {
+    https: fs.existsSync("./localhost.key")
+      ? {
+          key: fs.readFileSync("./localhost.key"),
+          cert: fs.readFileSync("./localhost.pem"),
+        }
+      : false,
+  },
 })
